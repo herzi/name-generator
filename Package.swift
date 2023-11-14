c41d3b0989d662134608b948187ada1490003837
Sven Herzberg <sven.herzberg@cluepunk.com>
2023-11-14 07:55:40 +0100

    📦 Generate Swift Package

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NameGenerator",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "NameGenerator",
+            targets: ["NameGenerator"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "NameGenerator"),
+        .testTarget(
+            name: "NameGeneratorTests",
+            dependencies: ["NameGenerator"]),
+    ]
+)
